73644fcdbf10d6cfda287e499924dd354d4d2c63
Hugo Trippaers <hugo@trippaers.nl>
2016-08-07 17:24:09 +0200

    Use a fixed crash URL

--- app/src/main/java/net/strocamp/hugo/wbfscoringscreen/ScreenApplication.java
+++ app/src/main/java/net/strocamp/hugo/wbfscoringscreen/ScreenApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import org.acra.ACRA;
 import org.acra.annotation.ReportsCrashes;
 
-@ReportsCrashes(formUri = "http://10.100.200.124:8080/rest-api/reportcrash", formKey = "test")
+@ReportsCrashes(formUri = "http://10.100.200.10:8080/rest-api/reportcrash", formKey = "test")
 public class ScreenApplication extends Application {
     @Override
     public void onCreate() {
